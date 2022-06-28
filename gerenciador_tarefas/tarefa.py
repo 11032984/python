1c9d1020210aef07ae4f28878d8d35b7e2e7687b
Maria Eduarda Gregorio <dudagbecker@gmail.com>
2022-06-27 22:51:48 -0300

    criando gerenciador para listar, adicionar e marcar concluida as tarefas

---
+++ gerenciador_tarefas/tarefa.py
@@ -0,0 +1,9 @@
+from xmlrpc.client import FastMarshaller
+
+class Tarefa:
+  def __init__(self, descricao):
+    self.descricao = descricao
+    self.concluida = False
+
+  def concluir(self):
+    self.concluida = True
