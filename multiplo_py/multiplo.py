fe1de6f04fedb09ef1f18ea5020ac7d7cdd78285
Maria Eduarda Gregorio <dudagbecker@gmail.com>
2022-06-27 22:57:46 -0300

    checando se um número é múltiplo de outro

---
+++ multiplo_py/multiplo.py
@@ -0,0 +1,24 @@
+# Crie um método que irá checar se um número é múltiplo de outro.
+# Retorne True caso seja múltiplo e False se não for.
+# Exemplos:
+
+# TODO: crie seu método aqui
+
+def multiplo(numero, multiplo):
+  if numero % multiplo == 0:
+    return True
+  else:
+    return False
+
+print(multiplo(355, 5))
+# True
+
+print(multiplo(356, 5))
+# False
+
+# print(multiplo_de(2357, 3))
+# False
+# print(multiplo_de(2358, 3))
+# True
+
+# TODO: crie seu método aqui
